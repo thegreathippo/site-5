d0b43a6f7323e7a569e6df7c17f93ac2c3e0aed8
thegreathippo <thegreathippo@gmail.com>
2018-09-23 22:03:08 -0400

    Instituted GameClock, tests for it.

--- site-5/game/clock.py
+++ site-5/game/clock.py
@@ -1,22 +1,74 @@
 from collections import namedtuple
 
-MINUTE = 0.00069444444
-HOUR = 0.04166666666
+DAY = 1440
+HOUR = 60
+YEAR = 525600
+LEAP_YEAR = 527040
+
+DAY_OF_MONTH = \
+    {1: 31,
+     2: 28,
+     3: 31,
+     4: 30,
+     5: 31,
+     6: 30,
+     7: 31,
+     8: 31,
+     9: 30,
+     10: 31,
+     11: 30,
+     12: 31}
+
+LEAP_DAY_OF_MONTH = \
+    {1: 31,
+     2: 29,
+     3: 31,
+     4: 30,
+     5: 31,
+     6: 30,
+     7: 31,
+     8: 31,
+     9: 30,
+     10: 31,
+     11: 30,
+     12: 31}
+
+DAYS_OF_WEEK = \
+    {0: "Monday",
+     1: "Tuesday",
+     2: "Wednesday",
+     3: "Thursday",
+     4: "Friday",
+     5: "Saturday",
+     6: "Sunday"}
+
+MONTHS = \
+    {1: "January",
+     2: "February",
+     3: "March",
+     4: "April",
+     5: "May",
+     6: "June",
+     7: "July",
+     8: "August",
+     9: "September",
+     10: "October",
+     11: "November",
+     12: "December"}
 
 
 class GameClock:
     """Base class for the in-game clock.
 
-    GameClock uses an internal decimal (_value) to track the in-game time. Anything to the left of the decimal point
-    represents the # of days since January 1st, 1900; anything to the right is the fraction of time that has passed
-    during the current day.
+    GameClock uses an internal timer (_timer) to track the time that has passed since the start of the
+    game (January 1st, 1900).
     """
 
-    def __init__(self, date_and_time=0.0):
-        self._value = date_and_time
+    def __init__(self, days=0):
+        self._timer = _Clock(days)
 
-    def advance(self, length=1.0):
-        self._value += length
+    def tick(self):
+        self._timer.advance()
 
     @property
     def date_and_time(self):
@@ -38,29 +90,106 @@ class GameClock:
 
     @property
     def year(self):
-        return 1900
+        return self._timer.year
 
     @property
     def month(self):
-        return 1
+        return self._timer.month
 
     @property
     def day(self):
-        return 1
+        return self._timer.day
 
     @property
     def hour(self):
-        return 0
+        return self._timer.hour
 
     @property
     def minute(self):
-        return 0
+        return self._timer.minute
 
     @property
     def named_day(self):
-        return "Monday"
+        return self._timer.named_day
 
     @property
     def named_month(self):
-        return "January"
+        return self._timer.named_month
 
+    @property
+    def total_days(self):
+        return self._timer.days
+
+
+class _Clock:
+
+    def __init__(self, days=0):
+        self._days = days
+        self.minute = 0
+        self.hour = 0
+        self.day, self.month, self.year = calculate_date(days)
+
+    def advance(self):
+        self.minute += 1
+        self._update()
+
+    def _update(self):
+        if self.minute == 60:
+            self.minute = 0
+            self.hour += 1
+        if self.hour == 24:
+            self.hour = 0
+            self.day += 1
+            self._days += 1
+        if is_leap_year(self.year):
+            day_of_month = LEAP_DAY_OF_MONTH
+        else:
+            day_of_month = DAY_OF_MONTH
+        if self.day > day_of_month[self.month]:
+            self.day = 1
+            self.month += 1
+        if self.month == 13:
+            self.month = 1
+            self.year += 1
+
+    @property
+    def named_day(self):
+        day = self._days % 7
+        return DAYS_OF_WEEK[day]
+
+    @property
+    def named_month(self):
+        return MONTHS[self.month]
+
+    @property
+    def days(self):
+        return self._days
+
+
+def is_leap_year(year):
+    if (year % 4) == 0:
+        if (year % 100) == 0:
+            if (year % 400) == 0:
+                return True
+            return False
+        return True
+
+
+def calculate_date(num_of_days):
+    day = 1
+    month = 1
+    year = 1900
+
+    for d in range(0, num_of_days):
+        day += 1
+        if is_leap_year(year):
+            day_of_month = LEAP_DAY_OF_MONTH
+        else:
+            day_of_month = DAY_OF_MONTH
+        if day > day_of_month[month]:
+            day = 1
+            month += 1
+        if month == 13:
+            month = 1
+            year += 1
+    return day, month, year
--- site-5/tests.py
+++ site-5/tests.py
@@ -4,7 +4,7 @@ import unittest
 
 class TestDayZero(unittest.TestCase):
     """Test the GameClock object at the start (January 1st, 1900)."""
-    start_at = 0.0
+    start_at = 0
     year = 1900
     month = 1
     day = 1
@@ -45,37 +45,60 @@ class TestDayZero(unittest.TestCase):
         self.assertEqual(time.minute, self.minute)
 
 
-class TestDayHalf(TestDayZero):
-    """Test the GameClock object 12 hours into Monday, January 1st, 1900."""
-    start_at = 0.5
-    hour = 12
-
-
 class TestDayOne(TestDayZero):
     """Test the GameClock object on the next day (January 2nd, 1900)."""
-    start_at = 1.0
+    start_at = 1
     day = 2
     named_day = "Tuesday"
 
 
 class TestDaySixty(TestDayZero):
-    """Test the GameClock object (its publicly available attributes and methods) sixty days later (Monday, March 2nd,
-    1900).
-    """
-    start_at = 60.0
-    month = 2
+    """Test the GameClock object sixty days later (Monday, March 2nd, 1900)."""
+    start_at = 60
+    month = 3
     day = 2
     named_month = "March"
-    named_day = "Monday"
+    named_day = "Friday"
 
 
 class TestDayTenThousand(TestDayZero):
-    start_at = 10000.0
+    """Test the GameClock object ten thousand days later (Friday, May 20th, 1927)."""
+    start_at = 10000
     year = 1927
     month = 5
     day = 20
     named_month = "May"
     named_day = "Friday"
+
+
+class TestDayFiftyThousand(TestDayZero):
+    """Test the GameClock object fifty thousand days later (Sunday, November 23rd, 2036)."""
+    start_at = 50000
+    year = 2036
+    month = 11
+    day = 23
+    named_month = "November"
+    named_day = "Sunday"
+
+
+class TestDayFiveHundredThousand(TestDayZero):
+    """Test the GameClock object five hundred thousand days later (Friday, December 14th, 3628)."""
+    start_at = 500000
+    year = 3268
+    month = 12
+    day = 14
+    named_month = "December"
+    named_day = "Friday"
+
+
+class TestAdvanceToDaySixty(TestDaySixty):
+    """Manually advance GameClock by sixty days (Monday, March 2nd, 1900) and then test it."""
+    start_at = 0
+
+    def setUp(self):
+        self.clock = clock.GameClock(self.start_at)
+        for m in range(0, (60 * 1440)):
+            self.clock.tick()
 
 
 unittest.main()
